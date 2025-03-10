e833ec8fe296010c036e183c996724473c681c73
Nicola Santacroce <nicola.santacroce@protonmail.com>
2025-03-10 16:04:37 +0100

    added template widget for file dialogs

--- src/napari_roxas_ai/__init__.py
+++ src/napari_roxas_ai/__init__.py
@@ -1,4 +1,8 @@
 __version__ = "0.0.1"
+from ._file_dialog_widget import FileDialog
 from ._sample_data import load_conifer_sample_data
 
-__all__ = ("load_conifer_sample_data",)
+__all__ = (
+    "load_conifer_sample_data",
+    "FileDialog",
+)
