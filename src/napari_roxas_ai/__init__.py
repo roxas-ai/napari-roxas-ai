eb0443c08f4aa9fdea70e83e90122a82af6a0284
Nicola Santacroce <nicola.santacroce@protonmail.com>
2025-03-24 15:46:53 +0100

    plugged in cells segmentation model

--- src/napari_roxas_ai/__init__.py
+++ src/napari_roxas_ai/__init__.py
@@ -1,8 +1,10 @@
 __version__ = "0.0.1"
 from ._measurements import CellsMeasurementsWidget
+from ._models import CellsModelWidget
 from ._sample_data import load_conifer_sample_data
 
 __all__ = (
-    "load_conifer_sample_data",
     "CellsMeasurementsWidget",
+    "CellsModelWidget",
+    "load_conifer_sample_data",
 )
---
+++ src/napari_roxas_ai/_models/__init__.py
@@ -0,0 +1,3 @@
+from ._cells_model_widget import CellsModelWidget
+
+__all__ = ("CellsModelWidget",)
