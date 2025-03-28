f0c8c78179d0e9e1a3beab1992943373537272e7
Nicola Santacroce <nicola.santacroce@protonmail.com>
2025-03-28 14:55:18 +0100

    changing test_deploy.yml

--- src/napari_roxas_ai/_tests/conftest.py
+++ src/napari_roxas_ai/_tests/conftest.py
@@ -3,17 +3,8 @@ Configure Qt for headless testing environments.
 """
 
 import os
-import sys
 
 
 def pytest_configure(config):
     """Force offscreen mode for headless Qt tests"""
     os.environ["QT_QPA_PLATFORM"] = "offscreen"
-
-    # Apply macOS-specific fixes without affecting Linux or Windows
-    if sys.platform == "darwin":
-        os.environ["QT_QPA_FONTDIR"] = "/System/Library/Fonts/Supplemental/"
-        os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = (
-            "/usr/local/Cellar/qt/6.*/plugins/platforms/"
-        )
-        os.environ["QT_DEBUG_PLUGINS"] = "1"  # Debugging (optional)
