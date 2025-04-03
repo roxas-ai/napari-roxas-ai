8bf33b7730867ef9ff6545f64c71fc85d0c93dfb
Nicola Santacroce <nicola.santacroce@protonmail.com>
2025-04-03 14:23:09 +0200

    added progress and scl output

--- src/napari_roxas_ai/_models/_cells_model_batch_widget.py
+++ src/napari_roxas_ai/_models/_cells_model_batch_widget.py
@@ -5,14 +5,17 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 from magicgui.widgets import (
+    CheckBox,
     ComboBox,
     Container,
+    ProgressBar,
     PushButton,
 )
 from PIL import Image
 from qtpy.QtCore import QObject, QThread, Signal
 from qtpy.QtWidgets import QFileDialog
 
+from .._utils import write_scl
 from ._cells_model import CellsSegmentationModel
 
 if TYPE_CHECKING:
@@ -28,15 +31,23 @@ excluded_path_words = [
 ]
 
 
+def save_as_scl(array, filepath):
+    """Save array as SCL file format."""
+    write_scl(array, filepath)
+
+
 class Worker(QObject):
     finished = Signal()
     result_ready = Signal(object)  # One images to send
+    progress = Signal(int, int)  # current, total
 
     def __init__(
         self,
         input_directory_path,
         output_directory_path: str,
         model_weights_file: str,
+        save_png: bool,
+        save_scl: bool,
     ):
         super().__init__()
         self.input_directory_path = input_directory_path
@@ -44,6 +55,8 @@ class Worker(QObject):
         self.model_weights_file = (
             f"{module_path}/_weights/{model_weights_file}"
         )
+        self.save_png = save_png
+        self.save_scl = save_scl
 
     def run(self):
         # Set up model
@@ -63,16 +76,26 @@ class Worker(QObject):
             for f in files_in
         ]
 
-        # Perform inference on images, and save the outputs as .png files
-        for _image_num, (img_path, out_path) in enumerate(
-            zip(files_in, files_out)
-        ):
+        # Perform inference on images, and save the outputs
+        total = len(files_in)
+        for i, (img_path, out_path) in enumerate(zip(files_in, files_out)):
+            # Emit progress signal
+            self.progress.emit(i, total)
+
             os.makedirs(os.path.dirname(out_path), exist_ok=True)
             with Image.open(img_path) as img:
                 img_array = np.array(img)
             prediction = model.infer(img_array)
-            Image.fromarray(prediction.astype("uint8")).save(f"{out_path}.png")
 
+            if self.save_png:
+                Image.fromarray(prediction.astype("uint8")).save(
+                    f"{out_path}.png"
+                )
+            if self.save_scl:
+                save_as_scl(prediction, f"{out_path}.scl")
+
+        # Final progress update
+        self.progress.emit(total, total)
         self.finished.emit()
 
 
@@ -103,6 +126,19 @@ class CellsModelBatchWidget(Container):
             label="Model Weights",
         )
 
+        # Add checkboxes for file saving options
+        self.save_png_checkbox = CheckBox(
+            value=True, label="Save output files as .png"
+        )
+        self.save_scl_checkbox = CheckBox(
+            value=False, label="Save output files as .scl"
+        )
+
+        # Add a progress bar with a description
+        self.progress_bar = ProgressBar(
+            value=0, min=0, max=100, visible=False, label="Progress"
+        )
+
         # Create a button to launch the analysis
         self._run_analysis_button = PushButton(text="Run Model")
         self._run_analysis_button.changed.connect(self._run_analysis)
@@ -113,7 +149,10 @@ class CellsModelBatchWidget(Container):
                 self._input_file_dialog_button,
                 self._output_file_dialog_button,
                 self.model_weights_file,
+                self.save_png_checkbox,
+                self.save_scl_checkbox,
                 self._run_analysis_button,
+                self.progress_bar,
             ]
         )
 
@@ -161,18 +200,37 @@ class CellsModelBatchWidget(Container):
             self.input_directory_path,
             self.output_directory_path,
             self.model_weights_file.value,
+            self.save_png_checkbox.value,
+            self.save_scl_checkbox.value,
         )
+
+    def _update_progress(self, current, total):
+        """Update the progress bar."""
+        if total > 0:
+            percentage = int(100 * current / total)
+            self.progress_bar.value = percentage
+            self.progress_bar.visible = True
 
     def _run_in_thread(
         self,
         input_directory_path: str,
         output_directory_path: str,
         model_weights_file: str,
+        save_png: bool,
+        save_scl: bool,
     ):
         """Run the analysis in a separate thread."""
+        # Reset and show progress bar
+        self.progress_bar.value = 0
+        self.progress_bar.visible = True
+
         self.worker_thread = QThread()
         self.worker = Worker(
-            input_directory_path, output_directory_path, model_weights_file
+            input_directory_path,
+            output_directory_path,
+            model_weights_file,
+            save_png,
+            save_scl,
         )
         self.worker.moveToThread(self.worker_thread)
 
@@ -181,5 +239,8 @@ class CellsModelBatchWidget(Container):
         self.worker.finished.connect(self.worker_thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.worker_thread.finished.connect(self.worker_thread.deleteLater)
+
+        # Connect progress signal
+        self.worker.progress.connect(self._update_progress)
 
         self.worker_thread.start()
--- src/napari_roxas_ai/_utils/__init__.py
+++ src/napari_roxas_ai/_utils/__init__.py
@@ -1,3 +1,8 @@
 from ._colormap_utils import make_binary_labels_colormap
+from ._scl_utils import read_scl, write_scl
 
-__all__ = ("make_binary_labels_colormap",)
+__all__ = (
+    "make_binary_labels_colormap",
+    "read_scl",
+    "write_scl",
+)
