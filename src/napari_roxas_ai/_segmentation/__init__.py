a111b7d8834266c7e064bb73ac2227b12004e235
Nicola Santacroce <nicola.santacroce@protonmail.com>
2025-05-17 12:16:54 +0200

    untracked *.pth and added download from release

--- src/napari_roxas_ai/_segmentation/__init__.py
+++ src/napari_roxas_ai/_segmentation/__init__.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+from napari_roxas_ai._assets_files import check_assets_and_download
+
 from ._batch_sample_segmentation import BatchSampleSegmentationWidget
 from ._single_sample_segmentation import SingleSampleSegmentationWidget
 
@@ -5,3 +9,12 @@ __all__ = [
     "SingleSampleSegmentationWidget",
     "BatchSampleSegmentationWidget",
 ]
+
+
+BASE_DIR = Path(__file__).parent.absolute()
+check_assets_and_download(
+    str(BASE_DIR / "_models" / "_cells"), "cells_models.zip"
+)
+check_assets_and_download(
+    str(BASE_DIR / "_models" / "_rings"), "rings_models.zip"
+)
