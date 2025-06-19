b43e5924db88d129a0d942ceb5c85dbd1c483378
Nicola Santacroce <83542144+tha-santacruz@users.noreply.github.com>
2025-06-19 13:54:22 +0200

    Update __init__.py
    version 0.1.2

--- src/napari_roxas_ai/__init__.py
+++ src/napari_roxas_ai/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 from ._conversion import cells_vectorization_widget
 from ._crossdating import CrossDatingPlotterWidget
 from ._edition import CellsLayerEditorWidget, RingsLayerEditorWidget
