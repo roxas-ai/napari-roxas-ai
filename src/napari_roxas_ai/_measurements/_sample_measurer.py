4564f24ae54928c1b2c9c835bb80f59bbd44c07a
severingoddon <severin.goddon@gmail.com>
2026-01-23 16:20:43 +0100

    add comment

--- src/napari_roxas_ai/_measurements/_sample_measurer.py
+++ src/napari_roxas_ai/_measurements/_sample_measurer.py
@@ -675,7 +675,7 @@ class SampleAnalyzer:
         """Return results as pandas DataFrame."""
         self.cells_table = pd.DataFrame(self.cells).T.set_index("id")
 
-    # TODO review needed!
+    # TODO review needed and add ll_scaling etc. to settings json
     def _apply_cwt_filters(self) -> None:
         """
         Automatic filtering of cell wall thickness (CWT) measurements.
